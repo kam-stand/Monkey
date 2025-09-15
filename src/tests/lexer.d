1cfa7c52241d29d3d8686d4164b853d87310f186
kamstand <kh84590@gmail.com>
2025-09-15 09:52:17 -0400

    basic tests for lexer and tokens

---
+++ src/tests/lexer.d
@@ -0,0 +1,71 @@
+module tests.lexer;
+import tokens.token;
+import lex.lexer;
+
+unittest
+{
+    string content = "{}()+/*-;,";
+    auto source_file = cast(ubyte[]) content;
+    Lexer lexer = new Lexer(source_file);
+
+    assert(lexer.source_file !is null); // MUST INSTANTIATE WITH SOURCE_FILE
+    assert(lexer.index == 0);
+}
+
+unittest
+{
+    string content = "{}()+/*-   ;,";
+    auto source_file = cast(ubyte[]) content;
+    Lexer lexer = new Lexer(source_file);
+
+    auto tokens = lexer.lexSource();
+    assert(tokens !is null);
+    assert(tokens.length == 10); // MUST HAVE EOF
+}
+
+unittest
+{
+    string content = "{(!=;,";
+    auto source_file = cast(ubyte[]) content;
+    Lexer lexer = new Lexer(source_file);
+
+    auto tokens = lexer.lexSource();
+    assert(tokens !is null);
+    assert(tokens.length == 7);
+    assert(tokens[2].type == TokenType.Bang);
+    assert(tokens[2].literal == "!");
+
+    assert(tokens[5].type == TokenType.Comma);
+    assert(tokens[5].literal == ",");
+
+}
+
+unittest
+{
+    string content = "return \"hello\";";
+    auto source_file = cast(ubyte[]) content;
+    Lexer lexer = new Lexer(source_file);
+
+    auto tokens = lexer.lexSource();
+    assert(tokens !is null);
+    assert(tokens.length == 4);
+    assert(tokens[1].type == TokenType.String);
+    assert(tokens[1].literal == "hello");
+    assert(tokens[1].literal.length == "hello".length);
+    assert(tokens[0].type == TokenType.Return);
+    assert(tokens[0].literal == "return");
+
+}
+
+unittest
+{
+    string content = "let x = 12;";
+    auto source_file = cast(ubyte[]) content;
+    Lexer lexer = new Lexer(source_file);
+
+    auto tokens = lexer.lexSource();
+    assert(tokens.length == 5);
+    assert(tokens[0].type == TokenType.Let); // MUST BE IDENT
+    assert(tokens[3].type == TokenType.Int); // MUST BE Int
+
+}
--- src/tests/token.d
+++ src/tests/token.d
@@ -3,5 +3,28 @@ import tokens.token;
 
 unittest
 {
+    auto t = initToken(TokenType.Int, "12");
+
+    static assert(is(typeof(t) == Token*)); // compile-time type check
+
+    assert(t.type == TokenType.Int); // runtime semantic check
+    assert(t.literal == "12");
+}
+
+unittest
+{
+    auto t = initToken(TokenType.Ident, "X");
+
+    assert((t.type == TokenType.Ident));
+    assert(t.literal.length > 0);
+
+}
+
+unittest
+{
+    auto t = initToken(TokenType.Eof, "");
+
+    assert((t.type == TokenType.Eof));
+    assert(t.literal.length == 0);
 
 }
