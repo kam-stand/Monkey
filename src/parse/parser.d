9a56663015be665960b61c2cc7edcb13c2dfff60
kamstand <kh84590@gmail.com>
2025-09-15 18:55:52 -0400

    feat: skeleton parser class

--- src/parse/parser.d
+++ src/parse/parser.d
@@ -1,1 +1,53 @@
 module parse.parser;
+import tokens.token;
+import syntax.statements;
+import syntax.program;
+
+class Parser
+{
+    Token*[] tokens;
+    int index = 0;
+
+    this(Token*[] tokens)
+    {
+        this.tokens = tokens;
+    }
+
+    Token* peek()
+    {
+        return tokens[index];
+    }
+
+    bool isAtEnd()
+    {
+        return peek().type == TokenType.Eof;
+    }
+
+    Program parseProgram()
+    {
+        Statement*[] statements = [];
+
+        while (!isAtEnd())
+        {
+            statements ~= parseStatement();
+        }
+
+        return Program(statements);
+    }
+
+    Statement* parseStatement()
+    {
+        switch (peek().type)
+        {
+        case TokenType.Let:
+            break;
+        case TokenType.Return:
+            break;
+        default:
+            break;
+        }
+
+        return new Statement();
+    }
+
+}
